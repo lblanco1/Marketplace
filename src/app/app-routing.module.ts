4e69dbf70f5c64c0317c571e1ea9cad647df5713
Lucas Henrique Aparecido Blanco <lucas.blanco@venturus.org.br>
2022-05-30 09:01:38 -0300

    Feat: create screen login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)}
+  { path: '', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)}
 ];
 
 export const AppRoutingModule: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes, { useHash: true });
